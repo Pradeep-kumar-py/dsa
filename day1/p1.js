1322860282e30c37ecbfc9c128a3d51b10c3b720
Pradeep <pk4401128@gmail.com>
2025-05-04 11:07:00 +0530

    first commit

---
+++ day1/p1.js
@@ -0,0 +1,19 @@
+// sum of two integer
+
+let a = 5;
+let b = 10;
+
+let sum = a + b;
+console.log("Sum of two integer is: " + sum);
+
+//let sum string and number
+
+let c = 6
+let d = "5"
+
+console.log(typeof(c+d))
+
+console.log("1"-1)  // result is  0  because of type coerssion string convert to number
+
+// const a = prompt("hello world") // prompt always take value in string
+
