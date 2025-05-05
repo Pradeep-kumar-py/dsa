215f1236666037c25704e37d0172343f1d83ac92
Pradeep <pk4401128@gmail.com>
2025-05-06 00:31:11 +0530

    Add prompt-sync and implement basic user input functionality

--- day3/p1.js
+++ day3/p1.js
@@ -12,3 +12,12 @@ for (let index = 200; index > 99; index--) {
 for(start; end ; change){
 
 }
+
+ // while loop it is used when when iteration is unknown
+
+start;
+
+while(end){
+    // code
+    change
+}
---
+++ day4/p1.js
@@ -0,0 +1,9 @@
+
+let prompt = require('prompt-sync')()
+
+let a = prompt("enter a number: ")
+
+process.stdout.write("hello ")
+
+
+process.stdout.write("World")
