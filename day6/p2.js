dbb9fae7ad41ef590e8b9fb209af5885a0a29982
Pradeep <pk4401128@gmail.com>
2025-05-10 15:32:41 +0530

    Refactor array rotation implementation in p2.js to use the reverse method for improved efficiency

--- day6/p2.js
+++ day6/p2.js
@@ -40,14 +40,38 @@
 
 
 // better approach 
+// let k = 2
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// let temp = new Array(arr.length)
+
+// for(let i = 0; i < arr.length; i++){
+//     temp[i] = arr[(i+k) % arr.length ]
+// }
+
+// console.log(temp);
+
+
+
+// more better approach
+
 let k = 2
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-let temp = new Array(arr.length)
+reverse(0, k-1)
+reverse(k, arr.length-1)
+reverse(0, arr.length-1)
+console.log(arr);
 
-for(let i = 0; i < arr.length; i++){
-    temp[i] = arr[(i+k) % arr.length ]
+
+function reverse(i, j) {
+    while (i<j) {
+        let temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+        i++
+        j--
+    }
 }
-
-console.log(temp);
